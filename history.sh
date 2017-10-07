f3bf7419749ddc21634bfb13abb23ded1df7c4cf
miguelemosreverte <miguellemos95@gmail.com>
2017-10-07 16:46:01 -0300

    Removed sudo command from script

--- history.sh
+++ history.sh
@@ -2,5 +2,5 @@
 rpcgen -C add.x
 rpcgen -a -C add.x
 make -f Makefile.add
-sudo systemctl add-wants multi-user.target rpcbind
+systemctl add-wants multi-user.target rpcbind
 #sudo apt-get install rpcbind

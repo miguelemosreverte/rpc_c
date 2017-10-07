7a191f28ade3d1bd89ff5903ae4c924fe91e0f84
miguelemosreverte <miguellemos95@gmail.com>
2017-10-07 17:21:53 -0300

    Added an instalation script for the dependencies and added back the sudo command in rpcgen.

---
+++ install_dependencies.sh
@@ -0,0 +1,4 @@
+#install rpcbind
+sudo apt-get install rpcbind
+#install CUnit framework
+sudo apt-get install libcunit1 libcunit1-doc libcunit1-dev
--- rpcgen.sh
+++ rpcgen.sh
@@ -2,5 +2,4 @@
 rpcgen -C add.x
 rpcgen -a -C add.x
 make -f Makefile.add
-systemctl add-wants multi-user.target rpcbind
-#sudo apt-get install rpcbind
+sudo systemctl add-wants multi-user.target rpcbind
